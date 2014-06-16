239656f23f4d90a730abfa290aa922e67e801426
kamadrigal <karlamv93@gmail.com>
2014-06-16 03:46:34 -0600

    adsadsad

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/karla/appshop/platforms/android/ant-build/classes.dex : \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/BuildConfig.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/R$string.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/R$attr.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/R.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/R$xml.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/R$drawable.class \
+/home/karla/appshop/platforms/android/ant-build/classes/cr/tec/diseno/App.class \
+/home/karla/appshop/platforms/android/ant-build/dexedLibs/classes-8fbf072e300413f899162b0a7aaf5994.jar \
